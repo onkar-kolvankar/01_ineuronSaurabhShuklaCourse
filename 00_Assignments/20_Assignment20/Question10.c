f3317b7f655da939243e5ac4ed14600b13f6ea32
Onkar Kolvankar <44017121+onkar-kolvankar@users.noreply.github.com>
2023-05-13 23:22:07 +0530

    -added comments in Assignment 20

--- 00_Assignments/20_Assignment20/Question10.c
+++ 00_Assignments/20_Assignment20/Question10.c
@@ -11,7 +11,9 @@ int main()
     for(str_len ; str_ptr[str_len] ; str_len++); //Calculate string length
 
     for(int i = str_len-1 ; i >= 0 ; i--)
-        printf("%c",str_ptr[i]);
+        printf("%c",str_ptr[i]);        // str[i] == str_ptr[i]
+        // str[i] = *(str + i) => here str in *(str + i) denotes address of 1st character i.e &str[0]
+        
 
     
     return 0;
